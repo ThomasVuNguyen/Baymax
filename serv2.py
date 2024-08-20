2ab0342129c78614e002a539d6f6d4dc6a0282ce
thomas the maker <tungvunguyennguyen@gmail.com>
2024-08-20 02:32:33 +0100

    updating new feet & added right arm

--- serv2.py
+++ serv2.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
@@ -7,16 +8,22 @@ left_thigh_id = 0
 right_feet_id = 2
 left_feet_id = 3
 
+right_armpit_id = 4
+
 right_thigh_angle = 90
 left_thigh_angle = 90
-right_feet_angle = 80
-left_feet_angle = 100
+right_feet_angle = 90
+left_feet_angle = 90
+
+right_armpit_angle = 0
 
 default_angle = {
 right_thigh_id: right_thigh_angle,
 left_thigh_id: left_thigh_angle,
 right_feet_id: right_feet_angle,
-left_feet_id: left_feet_angle
+left_feet_id: left_feet_angle,
+
+right_armpit_id: right_armpit_angle
 }
 
 position = sys.argv[1]
@@ -29,13 +36,20 @@ def servo(id, angle):
 
 def stand_up():
   for id in default_angle.keys():
+    print(id)
     servo(id, 0)
 
+def left_leg_up(angle):
+  servo(left_thigh_id, angle)
+  servo(left_feet_id, angle)
+  servo(right_armpit_id, angle)
+
 def squat():
-  servo(right_thigh_id, 20)
-  servo(left_thigh_id, 20)
-  servo(right_feet_id, 20)
-  servo(left_feet_id, -20)
+  servo(right_thigh_id, 60)
+  servo(left_thigh_id, 60)
+  servo(right_feet_id, -60)
+  servo(left_feet_id, 60)
+
 if position == 'stand':
   print('standing up')
   stand_up()
@@ -43,3 +57,16 @@ if position == 'stand':
 if position == 'squat':
   print('squating')
   squat()
+
+if position == 'left-up':
+  print('lifting left leg')
+  angle = int(sys.argv[2])
+  left_leg_up(angle)
+
+if position == 'move':
+  print('alternating between squat and stand')
+  stand_up()
+  time.sleep(1)
+  squat()
+  time.sleep(1)
+  stand_up()
